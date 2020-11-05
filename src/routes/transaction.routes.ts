9ce2fdbc97b2d9d6426d047971bba75963463b6a
José Carlos Nascimento <jcwd@josecarloswd.com>
2020-11-04 21:07:25 -0300

    Andamento - Create ok

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -19,7 +19,19 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
